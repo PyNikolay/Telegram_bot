901110191697cdfef0acd4971a131e39c2b016bf
ZeeG7coder <alecsandrmih2@gmail.com>
2025-03-26 17:20:49 +0200

    Add aiogram

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
-import telebot
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+import asyncio
 
-TOKEN = "YOUR TOKEN"
-bot = telebot.TeleBot(TOKEN)
+TOKEN = "YOUR_BOT_TOKEN"
 
-@bot.message_handler(commands = ['start'])
-def hello(message):
-	bot.send_message(message.chat.id, "Hello! Join in our TG Channel @SIG_devs")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-bot.polling(non_stope=True)
+@dp.message_handler(commands=["start"])
+async def start_command(message: Message):
+    await message.answer("Hello! Join in our TG Channel @SIG_devs")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.delete_webhook())
+    executor.start_polling(dp, skip_updates=True)
