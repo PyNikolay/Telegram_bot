3269f36cdf8372909d46d736e07c1c80c251ba8f
PyNikolay <nikolay.kozlov2727@gmail.com>
2025-03-26 19:18:31 +0300

    Bot Update

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
-from aiogram.utils import executor
+from aiogram.filters import Command
 import asyncio
 
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "YOUR_TOKEN"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=["start"])
+@dp.message(Command("start"))
 async def start_command(message: Message):
     await message.answer("Hello! Join in our TG Channel @SIG_devs")
 
+async def main():
+	print("Бот запущен")
+	await bot.delete_webhook(drop_pending_updates=True)
+	await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(bot.delete_webhook())
-    executor.start_polling(dp, skip_updates=True)
-
-
-print("Add ZeeG")
+	asyncio.run(main())
