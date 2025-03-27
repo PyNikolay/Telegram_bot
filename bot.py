4216f8a706002ce30377c2c34806a7ea18b79c60
PyNikolay <nikolay.kozlov2727@gmail.com>
2025-03-27 15:27:41 +0300

    Bot InlineKeyboard update

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command
 import asyncio
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-TOKEN = "YOUR_TOKEN"
+TOKEN = "7938866753:AAE0yEJP4bWfaUpQlNL7Z8Xr_GU7VtGVbcI"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start_command(message: Message):
-    await message.answer("Hello! Join in our TG Channel @SIG_devs")
+	keyboard = InlineKeyboardMarkup(
+		inline_keyboard=[
+			[InlineKeyboardButton(text="Channel", url="https://t.me/SIG_devs")]
+		]
+	)
+	
+	await message.answer("Hello! Join in our TG Channel @SIG_devs", reply_markup=keyboard)
 
 async def main():
 	print("Бот запущен")
